623d8f78e678299849cc58995cb6674b593dbe6b
hochan222 <hochan049@gmail.com>
2021-10-07 15:01:50 +0900

    feat: migration config complete

--- TicTacToe_TS_hochan/webpack.config.js
+++ TicTacToe_TS_hochan/webpack.config.js
@@ -4,15 +4,17 @@ module.exports = {
     filename: './bundle.js'
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+      { test: /\.(t|j)sx?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+      { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" }
     ]
   },
   externals: {
     "react": "React",
     "react-dom": "ReactDOM",
   },
+  devtool: "source-map"
 }
